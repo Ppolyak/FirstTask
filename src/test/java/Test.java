f98246cedc90337b1111400dacc229c105e1201a
pavelpaliakou <pasha_polyakov8@mail.ru>
2022-10-10 20:19:23 +0300

    3 test cases added

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -23,8 +23,55 @@ public class Test {
         //если ошибка, то код после ассертов не выполняется, поэтому после ассертов лучше ничего не писать
     }
 
-    @AfterClass
-    public void closeBrowser(){
-        driver.quit();
+    @org.testng.annotations.Test
+    public void allFieldsTest(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.xpath("//input[@name='zip_code']")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys("Namwwwe");
+        driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys("Nameeee");
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("asdasdasd@mail.qw");
+        driver.findElement(By.xpath("//input[@name='password1']")).sendKeys("Qweqwe123");
+        driver.findElement(By.xpath("//input[@name='password2']")).sendKeys("Qweqwe123");
+        driver.findElement(By.xpath("//input[@value='Register']")).click();
+        String  successMessage = driver.findElement(By.xpath("//span[@class='confirmation_message']"))
+                .getText();
+        Assert.assertEquals(successMessage,"Account is created!");
     }
+
+    @org.testng.annotations.Test
+    public void differentPasswordFieldsTest(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.xpath("//input[@name='zip_code']")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+        driver.findElement(By.xpath("//input[@name='first_name']")).sendKeys("Namwwwe");
+        driver.findElement(By.xpath("//input[@name='last_name']")).sendKeys("Nameeee");
+        driver.findElement(By.xpath("//input[@name='email']")).sendKeys("asdasdasd@mail.qw");
+        driver.findElement(By.xpath("//input[@name='password1']")).sendKeys("Qweqwe123");
+        driver.findElement(By.xpath("//input[@name='password2']")).sendKeys("Qweqwe3333");
+        driver.findElement(By.xpath("//input[@value='Register']")).click();
+        String textError = driver.findElement(By.xpath("//span[@class='error_message']")).getText();
+        Assert.assertEquals(textError,"Oops, error on page. Some of your fields have invalid data or email " +
+                "was previously used");
+    }
+
+    @org.testng.annotations.Test
+    public void requiredDisplayedTest(){
+        System.setProperty("webdriver.chrome.driver", "src/test/resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.sharelane.com/cgi-bin/register.py");
+        driver.findElement(By.xpath("//input[@name='zip_code']")).sendKeys("12345");
+        driver.findElement(By.xpath("//input[@value='Continue']")).click();
+        String text = String.valueOf(driver.findElement(By.xpath("//span[@class='required_text']")).isDisplayed());
+
+    }
+
+//    @AfterClass
+//    public void closeBrowser(){
+//        driver.quit();
+//    }
 }
